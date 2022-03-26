5cb1b417e589e089267438dc50c230c2b2f1e1c0
code-art4 <ojotriumph4@gmail.com>
2022-03-26 15:17:08 +0100

    added header of my portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import CTA from './CTA';
+import HeaderSocials from './headerSocials';
+import './header.css';
+
+//image imports
+import ME from "../../assets/profile.png"
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Ojo Triumph</h1>
+        <h5 className="text-light">Frontend developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="image">
+          <img src={ME} alt="me" className="me" srcset="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
