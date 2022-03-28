a48848f025fbe0b25ec286d24183387b0753860c
code-art4 <ojotriumph4@gmail.com>
2022-03-28 08:27:20 +0100

    fixed the size of header buttons

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from "../../assets/cv.pdf"
 
 const CTA = () => {
   return (
-    <div className="cta">
+    <div className="cta" style={{fontSize: "1rem"}}>
         <a href={CV} download className="btn">Download CV</a>
         <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
